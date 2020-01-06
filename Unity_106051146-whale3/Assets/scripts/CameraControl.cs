93a603be2b20877f111529342451eb2c2baa0607
jaklw <55180688+jaklw@users.noreply.github.com>
2020-01-06 22:17:14 +0800

    1-10
    完成

--- Unity_106051146-whale3/Assets/scripts/CameraControl.cs
+++ Unity_106051146-whale3/Assets/scripts/CameraControl.cs
@@ -5,11 +5,13 @@ public class CameraControl : MonoBehaviour
     public Transform cam, target;
 
     public float speed = 3;
+    public float r_speed = 100;
 
     private void Update()
     {
         Vector3 pos = Vector3.Lerp(cam.position, target.position, 0.5f * Time.deltaTime * speed);
-
         cam.position = pos;
+        float h = Input.GetAxis("Horizontal");
+        cam.Rotate(0, r_speed * h * Time.deltaTime, 0);
     }
 }
---
+++ Unity_106051146-whale3/Assets/scripts/player.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Transform tran;
+    public Rigidbody rig;
+    public float speed;
+    public float turn;
+    public Animator ani;
+
+    public Rigidbody cat_rig;
+
+    void OnTriggerStay(Collider other)
+    {
+        print(other);
+        if (other.name == "劍" &&ani.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            
+            Physics.IgnoreCollision(other, GetComponent<Collider>());
+            other.GetComponent<HingeJoint>().connectedBody = cat_rig;
+        }
+    }
+    private void Update()
+    {
+        walk();
+        run();
+        attack();
+    }
+    
+    private void walk()
+    {
+        float v = Input.GetAxis("Vertical");
+        rig.AddForce(tran.forward * speed *v* Time.deltaTime);
+        float h = Input.GetAxis("Horizontal");
+        tran.Rotate(0, turn * h * Time.deltaTime, 0);
+
+        ani.SetBool("走路開關", v != 0);
+    }
+    private void run()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ani.SetBool("跑步開關", true);
+        }
+        else
+        {
+            ani.SetBool("跑步開關", false);
+        }
+    }
+    private void attack()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ani.SetTrigger("攻擊觸發");
+        }
+    }
+
+}
